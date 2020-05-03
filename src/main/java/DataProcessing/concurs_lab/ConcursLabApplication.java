ad2683de0a7eeef18f47632d1868bf1a19936ffc
Utilka <andriy.v.bolshakov@gmail.com>
2020-05-03 20:31:23 +0300

    init

---
+++ src/main/java/DataProcessing/concurs_lab/ConcursLabApplication.java
@@ -0,0 +1,13 @@
+package DataProcessing.concurs_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcursLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcursLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/DataProcessing/concurs_lab/ConcursLabApplicationTests.java
@@ -0,0 +1,13 @@
+package DataProcessing.concurs_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcursLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
