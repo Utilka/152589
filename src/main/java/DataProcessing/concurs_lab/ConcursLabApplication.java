24a299a76128f333b3e42826d479d094a16f990d
Utilka <andriy.v.bolshakov@gmail.com>
2020-05-03 20:57:45 +0300

    create first files

--- src/main/java/DataProcessing/concurs_lab/ConcursLabApplication.java
+++ src/main/java/DataProcessing/concurs_lab/ConcursLabApplication.java
@@ -1,13 +1,19 @@
 package DataProcessing.concurs_lab;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ConcursLabApplication {
+public class ConcursLabApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConcursLabApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConcursLabApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 
 }
