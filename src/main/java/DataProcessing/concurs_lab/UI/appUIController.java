9f400cbd1e195db833a212b8f5aca37d9cd98ad8
a y <storminside4766@gmail.com>
2020-05-26 23:55:08 +0300

    add display movies on site

--- src/main/java/DataProcessing/concurs_lab/UI/appUIController.java
+++ src/main/java/DataProcessing/concurs_lab/UI/appUIController.java
@@ -5,8 +5,11 @@
  */
 package DataProcessing.concurs_lab.UI;
 
+import DataProcessing.concurs_lab.Classes.Movie;
+import DataProcessing.concurs_lab.Repositories.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,12 +21,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class appUIController {
 
+    private final MovieRepository movieRepository;
     
     @Autowired
-    public appUIController() {}
+    public appUIController(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
   
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String showStartingPage() {
+    public String showStartingPage(Model model) {
+        
+        
+        
+        Iterable<Movie> movies = movieRepository.findAll();
+        model.addAttribute("movies", movies);
+        
+        System.out.println(movies.iterator().next().getPoster());
+        
         return "index";
     }
     
